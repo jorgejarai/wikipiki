0c00f398ad048a22e07135b497f1a9877080d181
Jorge Jara <jorgejarainostroza@gmail.com>
2021-09-17 20:05:22 -0300

    feat: Change name on Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 const Header = () => {
   return (
     <header className='flex items-center p-6'>
-      <div className='text-2xl font-semibold'>Wiki</div>
+      <div className='text-2xl font-semibold'>Wikipiki&trade;</div>
       <nav className='flex-grow flex'>
         <div className='flex-grow'></div>
         <div className='flex space-x-2'>
