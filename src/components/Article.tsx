bbaae3a3afbf72032ac6f4f2058de707af30bcae
Jorge Jara <jorgejarainostroza@gmail.com>
2021-09-17 20:05:08 -0300

    fix: Enable using no breadcrumbs on an Article

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -2,7 +2,7 @@ import Markdown from './Markdown';
 
 interface IProps {
   title: string;
-  breadcrumbs: string[];
+  breadcrumbs?: string[];
   content: string;
 }
 
@@ -10,16 +10,18 @@ const Article = ({ title, breadcrumbs, content }: IProps) => {
   return (
     <section className='flex-grow overflow-y-auto flex flex-col items-center'>
       <header className='w-full md:w-2/3 px-8 md:px-0'>
-        <div className='text-sm flex'>
-          {breadcrumbs.map((crumb, idx) => {
-            return (
-              <div key={idx} className='cursor-pointer'>
-                {idx !== 0 && <span className='text-gray-400 px-1'>/</span>}
-                <span>{crumb}</span>
-              </div>
-            );
-          })}
-        </div>
+        {breadcrumbs && (
+          <div className='text-sm flex'>
+            {breadcrumbs.map((crumb, idx) => {
+              return (
+                <div key={idx} className='cursor-pointer'>
+                  {idx !== 0 && <span className='text-gray-400 px-1'>/</span>}
+                  <span>{crumb}</span>
+                </div>
+              );
+            })}
+          </div>
+        )}
         <h1 className='pt-2 text-4xl font-bold'>{title}</h1>
       </header>
       <article className='pt-4 w-full md:w-2/3 px-8 md:px-0 text-justify'>
