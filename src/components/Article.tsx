f20e8a0684d6ccb87b8c0237f45d04fb3520322e
Jorge Jara <jorgejarainostroza@gmail.com>
2021-11-15 11:41:42 -0300

    refactor: Remove breadcrumbs from Article

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -2,26 +2,13 @@ import Markdown from './Markdown';
 
 interface IProps {
   title: string;
-  breadcrumbs?: string[];
   content: string;
 }
 
-const Article = ({ title, breadcrumbs, content }: IProps) => {
+const Article = ({ title, content }: IProps) => {
   return (
     <section className='flex-grow overflow-y-auto flex flex-col items-center'>
-      <header className='w-full md:w-2/3 px-8 md:px-0'>
-        {breadcrumbs && (
-          <div className='text-sm flex'>
-            {breadcrumbs.map((crumb, idx) => {
-              return (
-                <div key={idx} className='cursor-pointer'>
-                  {idx !== 0 && <span className='text-gray-400 px-1'>/</span>}
-                  <span>{crumb}</span>
-                </div>
-              );
-            })}
-          </div>
-        )}
+      <header className='flex flex-col md:flex-row md:items-center space-y-2 w-full md:w-2/3 px-8 md:px-0'>
         <h1 className='pt-2 text-4xl font-bold'>{title}</h1>
       </header>
       <article className='pt-4 w-full md:w-2/3 px-8 md:px-0 text-justify'>
