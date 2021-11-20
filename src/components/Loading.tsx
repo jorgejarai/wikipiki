aa56f4e70df74d02f0e19ec9e270f8ed783bbc88
Jorge Jara <jorgejarainostroza@gmail.com>
2021-11-20 18:40:25 -0300

    fix: Set window title for Loading component

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,19 +1,26 @@
+import Head from 'next/head';
+
 const Loading = () => {
   return (
-    <div className='w-screen h-screen flex items-center justify-center'>
-      <svg
-        style={{
-          stroke: 'rgba(31, 41, 55, 1)',
-          fill: 'white',
-          strokeWidth: 5,
-          width: 100,
-          height: 100,
-        }}
-        className='animate-spin'
-      >
-        <path d=' M 90 50 A 40 40 0 1 1 70 15' />
-      </svg>
-    </div>
+    <>
+      <Head>
+        <title>Wikipiki</title>
+      </Head>
+      <div className='w-screen h-screen flex items-center justify-center'>
+        <svg
+          style={{
+            stroke: 'rgba(31, 41, 55, 1)',
+            fill: 'white',
+            strokeWidth: 5,
+            width: 100,
+            height: 100,
+          }}
+          className='animate-spin'
+        >
+          <path d=' M 90 50 A 40 40 0 1 1 70 15' />
+        </svg>
+      </div>
+    </>
   );
 };
 
